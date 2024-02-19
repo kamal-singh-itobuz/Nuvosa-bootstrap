0b6836284ba3951efcaec838b797354caa3a6be4
Itobuz <itobuz@Itobuzs-MacBook-Pro.local>
2024-02-19 12:16:41 +0530

    Light mode dark mode complete except hamberger and take a tour svg

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const toggleBtn = document.querySelector('.moon-sun-toggle');
+const rightArrow = document.querySelectorAll('.right-arrow');
+
 toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains('dark-theme')) {
         toggleBtn.src = "./images/navbar/sun.svg";
+        rightArrow.forEach(arrow => arrow.src = "./images/management/right-arrow-white.svg");
     }
-    else toggleBtn.src = "./images/navbar/moon.svg";
+    else{
+        toggleBtn.src = "./images/navbar/moon.svg";
+        rightArrow.forEach(arrow => arrow.src = "./images/management/right-arrow.svg");
+    }
 })
